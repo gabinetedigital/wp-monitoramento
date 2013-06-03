4cdae0f192624d76636d283a8031001699e5fda3
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2013-06-03 11:26:34 -0300

    (fix) Buscar o item da timeline corretamente

--- wp-monitoramento.xmlrpc.php
+++ wp-monitoramento.xmlrpc.php
@@ -158,7 +158,7 @@ function monitoramento_getObraTimeline($args) {
     // Utilizado get_pages ao invés de get_posts pois get_pages retorna a arvore correta dos posts conforme hierarquia.
     if (isset($args[2])){
         $theid = $args[2];
-        $my_posts = get_pages("child_of=$the_parent&post_ID=$theid&post_type=$post_type&post_status=publish&orderby=post_date&order=desc");
+        $my_posts = get_pages("include=$theid&post_type=$post_type&post_status=publish&orderby=post_date&order=desc");
     }else{
         $my_posts = get_pages("child_of=$the_parent&post_type=$post_type&post_status=publish&orderby=post_date&order=desc");
     }
